1066709d2ecf7a39a687d637a440ba25f3a8d29a
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-18 05:14:05 +0300

    login functionality

--- Krucial/Krucial/Controllers/AuthController.cs
+++ Krucial/Krucial/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
 ﻿using Krucial_API.Data;
 using Krucial_API.Models;
 using Krucial_API.Models.Dto;
+using Krucial_API.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Krucial_API.Controllers;
 
@@ -29,5 +31,102 @@ public class AuthController : ControllerBase
         _roleManager = roleManager;
     }
 
-    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDTO)
+    {
+        ApplicationUser userFromDb = _db.ApplicationUsers
+            .FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower());
+
+        bool isValid = await _userManager.CheckPasswordAsync(userFromDb, loginRequestDTO.Password);
+
+        if (isValid == false)
+        {
+            _response.Result = new LoginResponseDTO();
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username or password is incorrect.");
+            return BadRequest();
+        }
+
+        // Generate JWT token
+
+        LoginResponseDTO loginResponse = new()
+        {
+            Email = userFromDb.Email,
+            Token = "test"
+        };
+
+        if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username or password is incorrect.");
+            return BadRequest(_response);
+        }
+
+        _response.StatusCode = HttpStatusCode.OK;
+        _response.IsSuccess = true;
+        _response.Result = loginResponse;
+        return Ok(_response);
+
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+    {
+        ApplicationUser userFromDb = _db.ApplicationUsers
+            .FirstOrDefault(u => u.UserName.ToLower() == registerRequestDTO.UserName.ToLower());
+
+        if (userFromDb != null)
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username already exists");
+            return BadRequest(_response);
+        }
+
+        ApplicationUser newUser = new()
+        {
+            UserName = registerRequestDTO.UserName,
+            Email = registerRequestDTO.UserName,
+            NormalizedEmail = registerRequestDTO.UserName,
+            Name = registerRequestDTO.Name
+
+        };
+        try
+        {
+            var result = await _userManager.CreateAsync(newUser, registerRequestDTO.Password);
+            if (result.Succeeded)
+            {
+                if (_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                    await _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+                }
+                if (registerRequestDTO.Role.ToLower() == SD.Role_Admin)
+                {
+                    await _userManager.AddToRoleAsync(newUser, SD.Role_Admin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        _response.StatusCode = HttpStatusCode.BadRequest;
+        _response.IsSuccess = false;
+        _response.ErrorMessages.Add("Error while registering");
+        return BadRequest(_response);
+    }
+
+
 }
--- Krucial/Krucial/Models/Dto/LoginRequestDTO.cs
+++ Krucial/Krucial/Models/Dto/LoginRequestDTO.cs
@@ -2,6 +2,6 @@
 
 public class LoginRequestDTO
 {
-    public string Email { get; set; }
-    public string Token { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
 }
