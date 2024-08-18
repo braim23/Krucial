c0462fa382d4c2ae96f9a7a341e51d98e4582cf6
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-19 02:48:41 +0300

    set up stripe

---
+++ Krucial/Krucial/Models/Pagination.cs
@@ -0,0 +1,8 @@
+﻿namespace Krucial_API.Models;
+
+public class Pagination
+{
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalRecords { get; set; }
+}
