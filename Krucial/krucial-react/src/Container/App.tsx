afa50d0880151b13fceec1e8829a16d44b2e7b33
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-19 06:50:58 +0300

    kickstarting api

--- Krucial/krucial-react/src/Container/App.tsx
+++ Krucial/krucial-react/src/Container/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Footer, Header } from '../Components/Layout';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() =>{
+    fetch("https://localhost:7110/api/Product").then((response) => response.json()).then((data)=>{
+      console.log(data);
+      
+
+    });
+
+  }, []);
   return (
     <div >
       <Header />
