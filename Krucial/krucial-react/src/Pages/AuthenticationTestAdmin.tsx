6821fd8a1057f4802e08b2db73e7650633b3d218
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-21 09:07:28 +0300

    added authorization with HOC

--- Krucial/krucial-react/src/Pages/AuthenticationTestAdmin.tsx
+++ Krucial/krucial-react/src/Pages/AuthenticationTestAdmin.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { withAdminAuth } from "../HOC";
 
 function AuthenticationTestAdmin() {
-  return (
-    <div>Admin only.</div>
-  )
+  return <div>Admin only.</div>;
 }
 
-export default AuthenticationTestAdmin
+export default withAdminAuth(AuthenticationTestAdmin);
