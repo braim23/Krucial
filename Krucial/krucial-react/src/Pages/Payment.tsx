61c0a0490e96750786a8a546515592ee60425086
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-22 07:09:56 +0300

    continue setting up stripe

--- Krucial/krucial-react/src/Pages/Payment.tsx
+++ Krucial/krucial-react/src/Pages/Payment.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
+import { OrderSummary } from "../Components/Page/Order";
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-  console.log(apiResult);
-  console.log(userInput);
-  
-  return <div>Payment</div>;
+  const stripePromise = loadStripe(
+    "pk_test_51OLDD4I6vB0t5H7qRlVdm2VsyZd5QPCDQIjbAOKpJ8NAQxK0tKlL6f1xq1JajkZ26rMIcrjilAsAD5r8BjNHV5hH00snbKmKGw"
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <div className="container m-5 p-5">
+        <div className="row">
+          <div className="col-md-7">
+            <OrderSummary />
+          </div>
+          <div className="col-md-5">
+            <PaymentForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
+  );
 }
 
 export default Payment;
