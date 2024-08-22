7cee5f7653fbf913d3b37f4016dd3ac07278b0ea
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-22 07:31:07 +0300

    finished design of payment and order summary with calculations

--- Krucial/krucial-react/src/Components/Page/Order/OrderSummary.tsx
+++ Krucial/krucial-react/src/Components/Page/Order/OrderSummary.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { orderSummaryProps } from "./orderSummaryProps";
+import cartItemModel from "../../../Interfaces/cartItemModel";
 
-function OrderSummary() {
+function OrderSummary({ data, userInput }: orderSummaryProps) {
   return (
-    <div>OrderSummary</div>
-  )
+    <div>
+      {" "}
+      <h3 className="text-success">Order Summary</h3>
+      <div className="mt-3">
+        <div className="border py-3 px-2">Name : {userInput.name} </div>
+        <div className="border py-3 px-2">Email : {userInput.email} </div>
+        <div className="border py-3 px-2">Phone : {userInput.phoneNumber} </div>
+        <div className="border py-3 px-2">
+          <h4 className="text-success">Products</h4>
+          <div className="p-3">
+            {data.cartItems.map((cartitem: cartItemModel, index: number) => {
+              return (
+                <div className="d-flex">
+                  <div className="d-flex w-100 justify-content-between">
+                    <p>{cartitem.product?.name}</p>
+                    <p>
+                      ${cartitem.product?.price} x {cartitem.quantity} =
+                    </p>
+                  </div>
+                  <p style={{ width: "70px", textAlign: "right" }}>
+                    ${(cartitem.product?.price ?? 0) * (cartitem.quantity ?? 0)}
+                  </p>
+                </div>
+              );
+            })}
+
+            <hr />
+            <h4 className="text-danger" style={{ textAlign: "right" }}>
+              ${data.cartTotal.toFixed(2)}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OrderSummary
+export default OrderSummary;
