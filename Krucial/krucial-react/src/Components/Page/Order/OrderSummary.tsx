cef0e0cf5a9aa49666160b8ec051ad6f4c36f514
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-24 13:11:54 +0300

    functional status update

--- Krucial/krucial-react/src/Components/Page/Order/OrderSummary.tsx
+++ Krucial/krucial-react/src/Components/Page/Order/OrderSummary.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { orderSummaryProps } from "./orderSummaryProps";
 import cartItemModel from "../../../Interfaces/cartItemModel";
 import { getStatusColor } from "../../../Helper";
@@ -6,11 +6,15 @@ import { useNavigate } from "react-router-dom";
 import { SD_Roles, SD_Status } from "../../../Utilitiy/SD";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../Storage/Redux/store";
+import { useUpdateOrderHeaderMutation } from "../../../Apis/orderApi";
+import { MainLoader } from "../Common";
 
 function OrderSummary({ data, userInput }: orderSummaryProps) {
   const navigate = useNavigate();
   const badgeTypeColor = getStatusColor(data.status!);
   const userData = useSelector((state: RootState) => state.authStore);
+  const [loading, setIsLoading] = useState(false);
+  const [updateOrderHeader] = useUpdateOrderHeaderMutation();
 
   const nextStatus: any =
     data.status! === SD_Status.CONFIRMED
@@ -21,59 +25,93 @@ function OrderSummary({ data, userInput }: orderSummaryProps) {
           color: "success",
           value: SD_Status.COMPLETED,
         };
+
+  const handleNextStatus = async () => {
+    setIsLoading(true);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: nextStatus.value,
+    });
+
+    setIsLoading(false);
+  };
+  const handleCancel = async () => {
+    setIsLoading(true);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: SD_Status.CANCELLED,
+    });
+    setIsLoading(false);
+  };
   return (
     <div>
       {" "}
-      <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-success">Order Summary</h3>
-        <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
-          {data.status}
-        </span>
-      </div>
-      <div className="mt-3">
-        <div className="border py-3 px-2">Name : {userInput.name} </div>
-        <div className="border py-3 px-2">Email : {userInput.email} </div>
-        <div className="border py-3 px-2">Phone : {userInput.phoneNumber} </div>
-        <div className="border py-3 px-2">
-          <h4 className="text-success">Products</h4>
-          <div className="p-3">
-            {data.cartItems?.map((cartitem: cartItemModel, index: number) => {
-              return (
-                <div className="d-flex" key={index}>
-                  <div className="d-flex w-100 justify-content-between">
-                    <p>{cartitem.product?.name}</p>
-                    <p>
-                      ${cartitem.product?.price} x {cartitem.quantity} =
-                    </p>
-                  </div>
-                  <p style={{ width: "70px", textAlign: "right" }}>
-                    ${(cartitem.product?.price ?? 0) * (cartitem.quantity ?? 0)}
-                  </p>
-                </div>
-              );
-            })}
-
-            <hr />
-            <h4 className="text-danger" style={{ textAlign: "right" }}>
-              ${data.cartTotal?.toFixed(2)}
-            </h4>
+      {loading && <MainLoader />}
+      {!loading && (
+        <>
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="text-success">Order Summary</h3>
+            <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
+              {data.status}
+            </span>
           </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-between align-items-center mt-3">
-        <button className="btn btn-secondary" onClick={() => navigate(-1)}>
-          Back
-        </button>
+          <div className="mt-3">
+            <div className="border py-3 px-2">Name : {userInput.name} </div>
+            <div className="border py-3 px-2">Email : {userInput.email} </div>
+            <div className="border py-3 px-2">
+              Phone : {userInput.phoneNumber}{" "}
+            </div>
+            <div className="border py-3 px-2">
+              <h4 className="text-success">Products</h4>
+              <div className="p-3">
+                {data.cartItems?.map(
+                  (cartitem: cartItemModel, index: number) => {
+                    return (
+                      <div className="d-flex" key={index}>
+                        <div className="d-flex w-100 justify-content-between">
+                          <p>{cartitem.product?.name}</p>
+                          <p>
+                            ${cartitem.product?.price} x {cartitem.quantity} =
+                          </p>
+                        </div>
+                        <p style={{ width: "70px", textAlign: "right" }}>
+                          $
+                          {(cartitem.product?.price ?? 0) *
+                            (cartitem.quantity ?? 0)}
+                        </p>
+                      </div>
+                    );
+                  }
+                )}
 
-        {userData.role == SD_Roles.ADMIN && (
-          <div>
-            <button className="btn btn-danger mx-2">Cancel</button>
-            <button className={`btn btn-${nextStatus.color}`}>
-              {nextStatus.value}
+                <hr />
+                <h4 className="text-danger" style={{ textAlign: "right" }}>
+                  ${data.cartTotal?.toFixed(2)}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-between align-items-center mt-3">
+            <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+              Back
             </button>
+
+            {userData.role == SD_Roles.ADMIN && (
+              <div>
+                <button className="btn btn-danger mx-2" onClick={handleCancel}>
+                  Cancel
+                </button>
+                <button
+                  className={`btn btn-${nextStatus.color}`}
+                  onClick={handleNextStatus}
+                >
+                  {nextStatus.value}
+                </button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
