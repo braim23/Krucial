0e86216e9b3e2f6b01e4a4d6407621b2d3444c29
braim23 <94292623+braim23@users.noreply.github.com>
2024-08-26 12:34:28 +0300

    validation

--- Krucial/krucial-react/src/Pages/Product/ProductUpsert.tsx
+++ Krucial/krucial-react/src/Pages/Product/ProductUpsert.tsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { inputHelper, toastNotify } from "../../Helper";
 
+const productData = {
+  name: "",
+  description: "",
+  specialTag: "",
+  category: "",
+  price: "",
+};
 function ProductUpsert() {
+  const [imageToBeStore, setImageToStore] = useState<any>();
+  const [imageToDisplay, setImageToDisplay] = useState<string>("");
+  const [productInputs, setProductInputs] = useState(productData);
+  const handleProductInput = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const tempData = inputHelper(e, productInputs);
+    setProductInputs(tempData);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    if (file) {
+      const imgType = file.type.split("/")[1];
+      const validImgTypes = ["jpeg", "jpg", "png"];
+
+      const isImageTypeValid = validImgTypes.filter((e) => {
+        return e === imgType;
+      });
+
+      if (file.size > 1000 * 1024) {
+        setImageToStore("");
+        toastNotify("File must be less than 1 MB", "error");
+        return;
+      } else if (isImageTypeValid.length === 0) {
+        setImageToStore("");
+        toastNotify("File type must be jpg, png or jpeg", "error");
+        return;
+      }
+    }
+  };
   return (
     <div className="container border mt-5 p-5">
       <h3 className="offset-2 px-2 text-success">Add Product</h3>
@@ -12,28 +53,48 @@ function ProductUpsert() {
               className="form-control"
               placeholder="Enter Name"
               required
+              name="name"
+              value={productInputs.name}
+              onChange={handleProductInput}
             />
             <textarea
               className="form-control mt-3"
               placeholder="Enter Description"
+              name="description"
+              rows={10}
+              value={productInputs.description}
+              onChange={handleProductInput}
             ></textarea>
             <input
               type="text"
               className="form-control mt-3"
               placeholder="Enter Special Tag"
+              name="specialTag"
+              value={productInputs.specialTag}
+              onChange={handleProductInput}
             />
             <input
               type="text"
               className="form-control mt-3"
               placeholder="Enter Category"
+              name="category"
+              value={productInputs.category}
+              onChange={handleProductInput}
             />
             <input
               type="number"
               className="form-control mt-3"
               required
               placeholder="Enter Price"
+              name="price"
+              value={productInputs.price}
+              onChange={handleProductInput}
             />
-            <input type="file" className="form-control mt-3" />
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="form-control mt-3"
+            />
             <div className="text-center">
               <button
                 type="submit"
